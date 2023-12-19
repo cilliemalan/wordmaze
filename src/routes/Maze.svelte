d1c630980c0ea47f269f4bd4ac38a7ff6809c07c
Cillié Malan <cilliemalan@gmail.com>
2023-12-19 18:43:57 +0200

    dummy maze component

---
+++ src/routes/Maze.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { onMount } from "svelte";
+    import { generate } from "../lib/generation"
+
+    /** @type {string} */
+    export let text = "";
+
+    /** @type {HTMLCanvasElement} */
+    let canvasElement;
+
+    onMount(() => {
+        const ctx = canvasElement.getContext("2d");
+    });
+</script>
+
+<canvas bind:this={canvasElement}></canvas>
