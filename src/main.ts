2bfcd7808cecad3982f9cd44aa51350da83fe1bb
Cillié Malan <cilliemalan@gmail.com>
2023-12-22 10:08:28 +0200

    Maze solving

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import './style.css'
-import { generate, draw, type MazeData } from './maze'
+import { generate, draw, type MazeData, drawSolution, MazeDataWithStartAndEnd } from './maze'
 
 const canvas = document.querySelector("canvas")!;
 const ctx = canvas.getContext("2d")!;
@@ -7,7 +7,7 @@ const ctx = canvas.getContext("2d")!;
 const mazew = 60;
 const mazeh = 30;
 
-let maze: MazeData;
+let maze: MazeDataWithStartAndEnd;
 
 function generateMaze() {
     maze = generate({
@@ -21,26 +21,33 @@ function generateMaze() {
 function drawMaze() {
     const w = innerWidth;
     const h = innerHeight;
+    const mw = maze.bounds.w;
+    const mh = maze.bounds.h;
     canvas.width = w;
     canvas.height = h;
 
     ctx.fillStyle = "rgb(255, 255, 255)";
     ctx.fillRect(0, 0, w, h);
 
-    ctx.strokeStyle = "rgb(0,0,0)";
     ctx.fillStyle = "rgb(255,0,0)";
     ctx.lineWidth = 1;
 
-    let scale = w / (maze.width + 0.5);
-    if ((maze.height + 0.5) * scale > h) {
-        scale = h / (maze.height + 0.5);
+    let scale = w / (mw + 0.5);
+    if ((mh + 0.5) * scale > h) {
+        scale = h / (mh + 0.5);
     }
-    let xoff = -(((maze.width + 0.5) * scale) - w) / 2;
-    let yoff = -(((maze.height + 0.5) * scale) - h) / 2;
+    let xoff = -(((mw + 0.5) * scale) - w) / 2;
+    let yoff = -(((mh + 0.5) * scale) - h) / 2;
     ctx.translate(xoff, yoff);
     ctx.scale(scale, scale);
     ctx.lineWidth = 0.5;
+
+    ctx.strokeStyle = "rgb(0,0,0)";
     draw(ctx, maze);
+
+    ctx.strokeStyle = "rgb(0,0,255)";
+    ctx.lineWidth = 0.25;
+    drawSolution(ctx, maze);
 }
 
 addEventListener('resize', () => {
--- src/maze.ts
+++ src/maze.ts
@@ -21,12 +21,16 @@ export interface Block {
 }
 
 export interface MazeData {
+    data: Uint8Array;
+    stride: number;
+    bounds: Block;
+}
+
+export interface MazeDataWithStartAndEnd
+    extends MazeData {
+
     start: Point;
     end: Point;
-    width: number;
-    height: number;
-    stride: number;
-    data: Uint8Array;
 }
 
 export interface MazeOptions {
@@ -38,36 +42,10 @@ export interface MazeOptions {
     seed: string;
 }
 
-function topLeft({ x, y }: Block): Point {
-    return [x, y];
-}
-
-function bottomRight({ x, y, w, h }: Block): Point {
-    return [x + w, y + h];
-}
-
 function constrainPoint(m: MazeData, p: Point): Point {
-    const x = p[0] < 0 ? 0 :
-        p[0] >= m.width ? m.width - 1 :
-            p[0];
-
-    const y = p[1] < 0 ? 0 :
-        p[1] >= m.width ? m.width - 1 :
-            p[1];
-
+    const x = Math.max(Math.min(m.bounds.w - 1, p[0]), 0);
+    const y = Math.max(Math.min(m.bounds.h - 1, p[1]), 0);
     return [x, y];
-}
-
-function comparePoint(a: Point, b: Point): number {
-    if (a[0] < b[0])
-        return -1;
-    if (a[0] > b[0])
-        return 1;
-    if (a[1] < b[1])
-        return -1;
-    if (a[1] > b[1])
-        return 1;
-    return 0;
 }
 
 function advance(p: Point, edge: number): Point {
@@ -89,79 +67,82 @@ function advance(p: Point, edge: number): Point {
     }
 }
 
+function cellAddress(m: MazeData, p: Point) {
+    const [x, y] = p;
+    return (y + m.bounds.y) * m.stride + x + m.bounds.x;
+}
+
 function setCell(m: MazeData, p: Point, value: number) {
-    const [x, y] = constrainPoint(m, p);
-    m.data[y * m.stride + x] = value;
+    m.data[cellAddress(m, p)] = value;
 }
 
 function getCell(m: MazeData, p: Point): number {
-    const [x, y] = constrainPoint(m, p);
-    return m.data[y * m.stride + x];
-}
-
-function getEdge(m: MazeData, p: Point, edge: number): boolean {
-    return (getCell(m, p) & edge) != 0;
+    return m.data[cellAddress(m, p)];
 }
 
 function isInside(m: MazeData, p: Point, edge?: number): boolean {
     if (edge !== undefined) {
-        p = advance(p, edge);
+        return isInside(m, advance(p, edge), undefined);
     }
 
-    if (p[0] < 0 || p[1] < 0) {
+    const [x, y] = p;
+
+    if (x < 0 || y < 0) {
         return false;
     }
-    if (p[0] >= m.width || p[1] >= m.height) {
+
+    const { w, h } = m.bounds;
+    if (x >= w || y >= h) {
         return false;
     }
     return true;
 }
 
 function removeEdge(m: MazeData, p: Point, edge: number) {
-    const { width, height } = m;
-    const [x, y] = p;
+    const ap = advance(p, edge);
     const ee = ~edge;
     setCell(m, p, getCell(m, p) & ee);
-    if (x > 0 && (edge & L)) {
-        setCell(m, [x - 1, y], getCell(m, [x - 1, y]) & ~R);
+    if ((edge & L) && isInside(m, ap)) {
+        setCell(m, ap, getCell(m, ap) & ~R);
     }
-    if (x < width - 1 && (edge & R)) {
-        setCell(m, [x + 1, y], getCell(m, [x + 1, y]) & ~L);
+    if ((edge & R) && isInside(m, ap)) {
+        setCell(m, ap, getCell(m, ap) & ~L);
     }
-    if (y > 0 && (edge & T)) {
-        setCell(m, [x, y - 1], getCell(m, [x, y - 1]) & ~B);
+    if ((edge & T) && isInside(m, ap)) {
+        setCell(m, ap, getCell(m, ap) & ~B);
     }
-    if (y < height - 1 && (edge & B)) {
-        setCell(m, [x, y + 1], getCell(m, [x, y + 1]) & ~T);
+    if ((edge & B) && isInside(m, ap)) {
+        setCell(m, ap, getCell(m, ap) & ~T);
     }
 }
 
 function makeGap(m: MazeData, p: Point) {
     const [x, y] = p;
+    const { w, h } = m.bounds;
     if (x == 0) {
         removeEdge(m, p, L);
     }
-    else if (x == m.width - 1) {
+    else if (x == w - 1) {
         removeEdge(m, p, R);
     }
     else if (y == 0) {
         removeEdge(m, p, T);
     }
-    else if (y == m.height - 1) {
+    else if (y == h - 1) {
         removeEdge(m, p, B);
     }
 }
 
 function initializeMaze(m: MazeData) {
-    const { width, stride, height, data } = m;
-    for (let y = 0; y < height; y++) {
-        for (let x = 0; x < width; x++) {
-            data[y * stride + x] = FILLED;
+    const { w, h } = m.bounds;
+    for (let y = 0; y < h; y++) {
+        for (let x = 0; x < w; x++) {
+            setCell(m, [x, y], FILLED);
         }
     }
 }
 
-export function generate(options: MazeOptions): MazeData {
+export function generate(options: MazeOptions): MazeDataWithStartAndEnd {
     const width = Math.min(Math.max(options.width, MIN_WIDTH), MAX_WIDTH);
     const height = Math.min(Math.max(options.height, MIN_HEIGHT), MAX_HEIGHT);
     const stride = width;
@@ -169,7 +150,8 @@ export function generate(options: MazeOptions): MazeData {
     const start = options.start ?? [0, 0]
     const end = options.end ?? [width - 1, height - 1];
     const rng = new Prando(options.seed ?? "seed");
-    const m = { width, height, stride, data, start, end };
+    const bounds = { w: width, h: height, x: 0, y: 0 };
+    const m = { bounds, stride, data, start, end };
     initializeMaze(m);
     build(m, rng);
     makeGap(m, m.start);
@@ -178,7 +160,11 @@ export function generate(options: MazeOptions): MazeData {
 }
 
 function build(m: MazeData, rng: Prando) {
-    const s = [m.start];
+    const start: Point = [
+        Math.trunc(rng.next(0, m.bounds.w)),
+        Math.trunc(rng.next(0, m.bounds.h))
+    ];
+    const s = [start];
 
     while (s.length > 0) {
         const p = s.pop()!;
@@ -200,12 +186,87 @@ function build(m: MazeData, rng: Prando) {
     }
 }
 
+function solve(m: MazeData, start: Point, end: Point): Point[] | undefined {
+    interface VisitPoint {
+        point: Point;
+        parent?: VisitPoint;
+    }
+
+    const r = { point: start };
+    const q = new Array<VisitPoint>();
+    const x = new Set<number>();
+
+    visit(r);
+    enqueue(r);
+    while (q.length != 0) {
+        const v = dequeue()!;
+
+        if (v.point[0] == end[0] && v.point[1] == end[1]) {
+            return buildSolution(v);
+        }
+
+        const p = getCell(m, v.point);
+
+        for (let i = 0; i < 4; i++) {
+            const edge = 1 << i;
+            if (!isInside(m, v.point, edge)) {
+                continue;
+            }
+            if (p & edge) {
+                continue;
+            }
+            const w: VisitPoint = { point: advance(v.point, edge) };
+            if (visited(w)) {
+                continue;
+            }
+            visit(w);
+            w.parent = v;
+            enqueue(w);
+        }
+    }
+
+    return undefined;
+
+    function buildSolution(p: VisitPoint | undefined): Point[] {
+        const result = new Array<Point>();
+        while (p) {
+            result.push(p.point);
+            p = p.parent;
+        }
+
+        result.reverse();
+        return result;
+    }
+
+    function enqueue(p: VisitPoint) {
+        q.push(p);
+    }
+
+    function dequeue(): VisitPoint | undefined {
+        return q.shift();
+    }
+
+    function visit(p: VisitPoint) {
+        x.add(pspec(p));
+    }
+
+    function visited(p: VisitPoint) {
+        return x.has(pspec(p));
+    }
+
+    function pspec(p: VisitPoint) {
+        return p.point[0] << 16 | p.point[1];
+    }
+}
+
 export function draw(ctx: CanvasRenderingContext2D, m: MazeData) {
     const pf = ctx.lineWidth;
     const po = pf / 2;
+    const { w, h } = m.bounds;
     ctx.translate(po, po);
-    for (let y = 0; y < m.height; y++) {
-        for (let x = 0; x < m.width; x++) {
+    ctx.beginPath();
+    for (let y = 0; y < h; y++) {
+        for (let x = 0; x < w; x++) {
             const c = getCell(m, [x, y]);
             if (y == 0 && c & T) {
                 ctx.moveTo(x + 0 - po, y + 0);
@@ -224,6 +285,25 @@ export function draw(ctx: CanvasRenderingContext2D, m: MazeData) {
                 ctx.lineTo(x + 0, y + 0 - po);
             }
         }
+    }
+    ctx.stroke();
+    ctx.translate(-po, -po);
+}
+
+export function drawSolution(ctx: CanvasRenderingContext2D, m: MazeDataWithStartAndEnd) {
+    const solution = solve(m, m.start, m.end);
+    if (!solution || !solution.length) {
+        return;
+    }
+
+    const pf = ctx.lineWidth;
+    const po = 0.75;
+    ctx.translate(po, po);
+    ctx.beginPath();
+    ctx.moveTo(solution[0][0], solution[0][1]);
+    for (let i = 0; i < solution.length; i++) {
+        const p = solution[i];
+        ctx.lineTo(p[0], p[1]);
     }
     ctx.stroke();
     ctx.translate(-po, -po);
